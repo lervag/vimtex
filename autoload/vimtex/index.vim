lervag/vimtex

commit 7e2793d4e3b661a09c091ae30dd6b3162e9febe0

Cf. #532: Improved vimtex-include system

autoload/vimtex/index.vim
@@ -62,6 +62,7 @@ function! vimtex#index#create(index) " {{{1
     unlet FnVal
   endfor
 
+  let l:vimtex = b:vimtex
   if g:vimtex_index_split_pos ==# 'full'
     silent execute 'edit' escape(a:index.name, ' ')
   else
@@ -72,6 +73,7 @@ function! vimtex#index#create(index) " {{{1
           \ g:vimtex_index_split_pos g:vimtex_index_split_width
           \ 'new' escape(a:index.name, ' ')
   endif
+  let b:vimtex = l:vimtex
   let b:index = a:index
 
   setlocal bufhidden=wipe
@@ -134,6 +136,7 @@ function! s:actions_activate(close) dict "{{{1
   let n = getpos('.')[1] - 1
   if n < self.help_nlines | return | endif
   let entry = self.entries[n - self.help_nlines]
+  let l:vimtex_main = b:vimtex.tex
 
   " Save index buffer info for later use
   let toc_bnr = bufnr('%')
@@ -172,6 +175,12 @@ function! s:actions_activate(close) dict "{{{1
   " Go to entry line
   if has_key(entry, 'line')
     call setpos('.', [0, entry.line, 0, 0])
+  endif
+
+  " If relevant, enable vimtex stuff
+  if get(entry, 'link', 0)
+    let b:vimtex_main = l:vimtex_main
+    call vimtex#init()
   endif
 
   " Ensure folds are opened

autoload/vimtex/toc.vim
@@ -157,6 +157,7 @@ function! vimtex#toc#get_entries() " {{{1
       if l:fname[0] !=# '/'
         let l:fname = b:vimtex.root . '/' . l:fname
       endif
+      let l:fname = fnamemodify(l:fname, ':~:.')
       call add(l:toc, {
             \ 'title'  : 'Vimtex include: '
             \            . (strlen(l:fname) < 54
@@ -165,6 +166,7 @@ function! vimtex#toc#get_entries() " {{{1
             \ 'number' : '',
             \ 'file'   : l:fname,
             \ 'level'  : s:number.current_level,
+            \ 'link'   : 1,
             \ })
       continue
     endif

commit 18a2b062b8f8d6e2dfa2c481553b0181a594c85b

Fixed #594: Regression bug since 7e2793d4

autoload/vimtex/index.vim
@@ -62,7 +62,7 @@ function! vimtex#index#create(index) " {{{1
     unlet FnVal
   endfor
 
-  let l:vimtex = b:vimtex
+  let l:vimtex = get(b:, 'vimtex', {})
   if g:vimtex_index_split_pos ==# 'full'
     silent execute 'edit' escape(a:index.name, ' ')
   else
@@ -136,7 +136,7 @@ function! s:actions_activate(close) dict "{{{1
   let n = getpos('.')[1] - 1
   if n < self.help_nlines | return | endif
   let entry = self.entries[n - self.help_nlines]
-  let l:vimtex_main = b:vimtex.tex
+  let l:vimtex_main = get(b:vimtex, 'tex', '')
 
   " Save index buffer info for later use
   let toc_bnr = bufnr('%')
@@ -178,7 +178,7 @@ function! s:actions_activate(close) dict "{{{1
   endif
 
   " If relevant, enable vimtex stuff
-  if get(entry, 'link', 0)
+  if get(entry, 'link', 0) && !empty(l:vimtex_main)
     let b:vimtex_main = l:vimtex_main
     call vimtex#init()
   endif