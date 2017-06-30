lervag/vimtex

commit f74c984ca0f94a2887254765615b9cfe84a2ad09

Improved efficiency of parse_engine (cf. #874)

autoload/vimtex/state.vim
@@ -409,15 +409,11 @@ function! s:vimtex.parse_engine() abort dict " {{{1
         \ 'context (xetex)'  : '-pdflatex=''texexec --xtx''',
         \}
 
-  let self.engine = ''
+  let l:engines = copy(self.preamble[:20])
+  call map(l:engines, 'matchstr(v:val, l:engine_regex)')
+  call filter(l:engines, 'empty(v:val)')
 
-  for l:line in self.preamble
-    let l:engine = matchstr(l:line, l:engine_regex)
-    if !empty(l:engine)
-      let self.engine = get(l:engine_list, tolower(l:engine), '')
-      continue
-    endif
-  endfor
+  let self.engine = get(l:engine_list, tolower(get(l:engines, -1, 'pdflatex')), '')
 endfunction
 
 " }}}1

commit 0442ab4a4063375194e11643d4b068dca263cc3d

Fix s:vimtex.parse_engine()
It broke in f74c984.

autoload/vimtex/state.vim
@@ -415,7 +415,7 @@ function! s:vimtex.parse_engine() abort dict " {{{1
 
   let l:engines = copy(self.preamble[:20])
   call map(l:engines, 'matchstr(v:val, l:engine_regex)')
-  call filter(l:engines, 'empty(v:val)')
+  call filter(l:engines, '!empty(v:val)')
 
   let self.engine = get(l:engine_list, tolower(get(l:engines, -1, 'pdflatex')),
         \ get(get(b:, 'vimtex', {}), 'engine', ''))