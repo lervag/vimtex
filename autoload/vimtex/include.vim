lervag/vimtex

commit f21f4ae3db24990fbeb07841cdfcbf008ff8ffc8

Fix failing 'gf' on \bibliography macro

autoload/vimtex/include.vim
@@ -68,6 +68,10 @@ function! s:input(fname, type) abort " {{{1
   let l:file = substitute(l:file, '^\s*"\|"\s*$', '', 'g')
   let l:file = substitute(l:file, '\\space', '', 'g')
 
+  if strpart(l:file, -3) !=# a:type
+    let l:file .= '.' . a:type
+  endif
+
   return l:file
 endfunction
 

commit 41eb5b74b05dd604f4247175ecdd9c4a5e5a0fe1

fix: regression bug since f21f4ae
fixes: #1646

autoload/vimtex/include.vim
@@ -68,7 +68,7 @@ function! s:input(fname, type) abort " {{{1
   let l:file = substitute(l:file, '^\s*"\|"\s*$', '', 'g')
   let l:file = substitute(l:file, '\\space', '', 'g')
 
-  if strpart(l:file, -3) !=# a:type
+  if l:file[-3:] !=# a:type
     let l:file .= '.' . a:type
   endif
 