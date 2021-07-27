lervag/vimtex

commit b4fc8ae419252ee96e8eb7cf0076573443c20e81

fix(syntax): theorem env opts are in core
refer: #2086

autoload/vimtex/syntax/core.vim
@@ -395,6 +395,17 @@ function! vimtex#syntax#core#init() abort " {{{1
         \)
   call vimtex#syntax#core#new_opt('texNewthmOptNumberby')
 
+  " \begin{mytheorem}[custom title]
+  call vimtex#syntax#core#new_opt('texTheoremEnvOpt', {
+        \ 'contains': 'TOP,@NoSpell'
+        \})
+  for l:envname in s:gather_newtheorems()
+    execute 'syntax match texTheoremEnvBgn'
+          \ printf('"\\begin{%s}"', l:envname)
+          \ 'nextgroup=texTheoremEnvOpt skipwhite'
+          \ 'contains=texCmdEnv'
+  endfor
+
   " }}}2
   " {{{2 Comments
 
@@ -761,6 +772,7 @@ function! vimtex#syntax#core#init_highlights() abort " {{{1
   highlight def link texTabularChar        texSymbol
   highlight def link texTabularCol         texOpt
   highlight def link texTabularOpt         texEnvOpt
+  highlight def link texTheoremEnvOpt      texEnvOpt
   highlight def link texVerbZone           texZone
   highlight def link texVerbZoneInline     texVerbZone
 endfunction
@@ -1879,6 +1891,17 @@ function! s:match_conceal_cites_icon() abort " {{{1
   execute 'syntax match texCmdRefConcealed'
         \ '"\\cite[tp]\?\*\?\%(\[[^]]*\]\)\{,2}{[^}]*}"'
         \ 'conceal cchar=' . g:vimtex_syntax_conceal_cites.icon
+endfunction
+
+" }}}1
+
+function! s:gather_newtheorems() abort " {{{1
+  let l:lines = vimtex#parser#preamble(b:vimtex.tex)
+
+  call filter(l:lines, {_, x -> x =~# '^\s*\\newtheorem\>'})
+  call map(l:lines, {_, x -> matchstr(x, '^\s*\\newtheorem\>\*\?{\zs[^}]*')})
+
+  return l:lines
 endfunction
 
 " }}}1

autoload/vimtex/syntax/p/amsthm.vim
@@ -5,8 +5,6 @@
 "
 
 function! vimtex#syntax#p#amsthm#load(cfg) abort " {{{1
-  call s:gather_newtheorems()
-
   syntax match texCmdNewthm "\\newtheorem\*"
         \ nextgroup=texNewthmArgName skipwhite skipnl
 
@@ -18,31 +16,7 @@ function! vimtex#syntax#p#amsthm#load(cfg) abort " {{{1
         \ 'contains': 'TOP,@NoSpell'
         \})
 
-  call vimtex#syntax#core#new_opt('texTheoremEnvOpt', {
-        \ 'contains': 'TOP,@NoSpell'
-        \})
-
-  for l:envname in b:vimtex.syntax.amsthm
-    execute 'syntax match texTheoremEnvBgn'
-          \ printf('"\\begin{%s}"', l:envname)
-          \ 'nextgroup=texTheoremEnvOpt skipwhite'
-          \ 'contains=texCmdEnv'
-  endfor
-
   highlight def link texProofEnvOpt texEnvOpt
-  highlight def link texTheoremEnvOpt texEnvOpt
-endfunction
-
-" }}}1
-
-function! s:gather_newtheorems() abort " {{{1
-  if has_key(b:vimtex.syntax, 'amsthm') | return | endif
-
-  let l:lines = vimtex#parser#preamble(b:vimtex.tex)
-  let b:vimtex.syntax.amsthm = l:lines
-
-  call filter(l:lines, {_, x -> x =~# '^\s*\\newtheorem\>'})
-  call map(l:lines, {_, x -> matchstr(x, '^\s*\\newtheorem\>\*\?{\zs[^}]*')})
 endfunction
 
 " }}}1

test/test-syntax/test-amsthm.vim
@@ -6,8 +6,6 @@ set spell
 
 if empty($INMAKE) | finish | endif
 
-call assert_equal(3, len(b:vimtex.syntax.amsthm))
-
 call assert_true(vimtex#syntax#in('texNewthmOptNumberby', 4, 32))
 call assert_true(vimtex#syntax#in('texNewthmOptCounter', 5, 19))
 

test/test-syntax/test-core.vim
@@ -12,7 +12,7 @@ call assert_true(vimtex#syntax#in('texAuthorArg', 64, 20))
 call assert_true(vimtex#syntax#in('texDelim', 64, 39))
 
 call assert_true(vimtex#syntax#in('texNewthmArgPrinted', 38, 23))
-call assert_true(vimtex#syntax#in('texEnvOpt', 114, 18))
+call assert_true(vimtex#syntax#in('texTheoremEnvOpt', 114, 18))
 
 call assert_true(vimtex#syntax#in('texCmdBibitem', 119, 3))
 call assert_true(vimtex#syntax#in('texBibitemArg', 119, 13))

commit a8452e079e853d12fb3c29fae95ba17c5ba29138

fix: regression after b4fc8ae
refer: #2120, #2086

autoload/vimtex/syntax/core.vim
@@ -399,12 +399,12 @@ function! vimtex#syntax#core#init() abort " {{{1
   call vimtex#syntax#core#new_opt('texTheoremEnvOpt', {
         \ 'contains': 'TOP,@NoSpell'
         \})
-  for l:envname in s:gather_newtheorems()
-    execute 'syntax match texTheoremEnvBgn'
-          \ printf('"\\begin{%s}"', l:envname)
-          \ 'nextgroup=texTheoremEnvOpt skipwhite skipnl'
-          \ 'contains=texCmdEnv'
-  endfor
+  " for l:envname in s:gather_newtheorems()
+  "   execute 'syntax match texTheoremEnvBgn'
+  "         \ printf('"\\begin{%s}"', l:envname)
+  "         \ 'nextgroup=texTheoremEnvOpt skipwhite skipnl'
+  "         \ 'contains=texCmdEnv'
+  " endfor
 
   " }}}2
   " {{{2 Comments