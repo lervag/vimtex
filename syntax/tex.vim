lervag/vimtex

commit 00ed2f1612599c07de8d77cc85b4c1becfd1ae0f

fix(syntax): ensure init_post when syntax reloads
refer: #2165

syntax/tex.vim
@@ -23,6 +23,7 @@ call vimtex#syntax#core#init()
 call vimtex#syntax#core#init_highlights()
 
 " Initialize buffer local syntax state
+unlet! b:vimtex_syntax_did_postinit
 let b:vimtex_syntax = {}
 call vimtex#syntax#nested#reset()
 
@@ -38,9 +39,9 @@ endif
 " 2. that the init_post function is executed when VimTeX state is loaded (if it
 "    was not already done).
 augroup vimtex_syntax
-  autocmd!
-  autocmd User VimtexEventInitPost call vimtex#syntax#core#init_post()
-  autocmd ColorScheme * call vimtex#syntax#core#init_highlights()
+  autocmd! * <buffer>
+  autocmd User <buffer> VimtexEventInitPost call vimtex#syntax#core#init_post()
+  autocmd ColorScheme <buffer> * call vimtex#syntax#core#init_highlights()
 augroup END
 
 unlet s:is_loading

commit 9fa256152db3a15584b38b49b331d65322fa6215

fix: regression bug after 00ed2f1
refer: #2166

syntax/tex.vim
@@ -40,8 +40,8 @@ endif
 "    was not already done).
 augroup vimtex_syntax
   autocmd! * <buffer>
-  autocmd User <buffer> VimtexEventInitPost call vimtex#syntax#core#init_post()
   autocmd ColorScheme <buffer> * call vimtex#syntax#core#init_highlights()
+  autocmd! User VimtexEventInitPost call vimtex#syntax#core#init_post()
 augroup END
 
 unlet s:is_loading