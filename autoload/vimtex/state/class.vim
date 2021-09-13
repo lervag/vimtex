lervag/vimtex

commit 92d268ad74ebee209403c2a23d8c3ea30b47493f

chore: more refactoring

autoload/vimtex/state/class.vim
@@ -1,0 +1,247 @@
+" VimTeX - LaTeX plugin for Vim
+"
+" Maintainer: Karl Yngve Lervåg
+" Email:      karl.yngve@gmail.com
+"
+
+function! vimtex#state#class#new(main, main_parser, preserve_root) abort " {{{1
+  let l:new = deepcopy(s:vimtex)
+
+  let l:new.root = fnamemodify(a:main, ':h')
+  let l:new.base = fnamemodify(a:main, ':t')
+  let l:new.name = fnamemodify(a:main, ':t:r')
+  let l:new.main_parser = a:main_parser
+
+  if a:preserve_root && exists('b:vimtex')
+    let l:new.root = b:vimtex.root
+    let l:new.base = vimtex#paths#relative(a:main, l:new.root)
+  endif
+
+  let l:ext = fnamemodify(a:main, ':e')
+  let l:new.tex = l:ext ==# 'tex' ? a:main : ''
+
+  " Get preamble for some state parsing
+  let l:preamble = !empty(l:new.tex)
+        \ ? vimtex#parser#preamble(l:new.tex, {'root' : l:new.root})
+        \ : []
+
+  let l:new.tex_program = s:parse_tex_program(l:preamble)
+  let l:new.documentclass = s:parse_documentclass(l:preamble)
+  let l:new.packages = s:parse_packages(l:preamble)
+  let l:new.graphicspath = s:parse_graphicspath(l:preamble, l:new.root)
+  let l:new.sources = s:gather_sources(l:new.tex, l:new.root)
+
+  " Initialize state in submodules
+  let l:new.disabled_modules = get(s:, 'disabled_modules', [])
+  for l:mod in filter(
+        \ ['view', 'compiler', 'qf', 'toc', 'fold', 'context'],
+        \ 'index(l:new.disabled_modules, v:val) < 0')
+    call vimtex#{l:mod}#init_state(l:new)
+  endfor
+
+  return l:new
+endfunction
+
+" }}}1
+
+
+let s:vimtex = {}
+
+function! s:vimtex.__pprint() abort dict " {{{1
+  let l:items = [
+        \ ['name', self.name],
+        \ ['base', self.base],
+        \ ['root', self.root],
+        \ ['tex', self.tex],
+        \ ['main parser', self.main_parser],
+        \]
+
+  if exists('self.documentclass')
+    call add(l:items, ['document class', self.documentclass])
+  endif
+
+  if !empty(self.packages)
+    call add(l:items, ['packages', join(sort(keys(self.packages)))])
+  endif
+
+  if self.tex_program !=# '_'
+    call add(l:items, ['tex program', self.tex_program])
+  endif
+
+  if len(self.sources) >= 2
+    call add(l:items, ['source files', self.sources])
+  endif
+
+  call add(l:items, ['compiler', get(self, 'compiler', {})])
+  call add(l:items, ['viewer', get(self, 'viewer', {})])
+
+  if exists('self.qf.name')
+    call add(l:items, ['qf method', self.qf.name])
+  endif
+
+  return [['VimTeX project', l:items]]
+endfunction
+
+" }}}1
+
+function! s:vimtex.cleanup() abort dict " {{{1
+  if exists('self.compiler.is_running')
+        \ && self.compiler.is_running()
+    call self.compiler.kill()
+  endif
+
+  if exists('#User#VimtexEventQuit')
+    if exists('b:vimtex')
+      let b:vimtex_tmp = b:vimtex
+    endif
+    let b:vimtex = self
+    doautocmd <nomodeline> User VimtexEventQuit
+    if exists('b:vimtex_tmp')
+      let b:vimtex = b:vimtex_tmp
+      unlet b:vimtex_tmp
+    else
+      unlet b:vimtex
+    endif
+  endif
+
+  " Close quickfix window
+  silent! cclose
+endfunction
+
+" }}}1
+function! s:vimtex.getftime() abort dict " {{{1
+  return max(map(copy(self.sources), 'getftime(self.root . ''/'' . v:val)'))
+endfunction
+
+" }}}1
+function! s:vimtex.update_packages() abort dict " {{{1
+  " Try to parse .fls file if present, as it is usually more complete. That is,
+  " it contains a generated list of all the packages that are used.
+  for l:line in vimtex#parser#fls(self.fls())
+    let l:package = matchstr(l:line, '^INPUT \zs.\+\ze\.sty$')
+    let l:package = fnamemodify(l:package, ':t')
+    if !empty(l:package)
+      let self.packages[l:package] = {}
+    endif
+  endfor
+endfunction
+
+" }}}1
+
+function! s:vimtex.ext(ext, ...) abort dict " {{{1
+  " Check for various output directories
+  " * Environment variable VIMTEX_OUTPUT_DIRECTORY. Note that this overrides
+  "   any VimTeX settings like g:vimtex_compiler_latexmk.build_dir!
+  " * Compiler settings, such as g:vimtex_compiler_latexmk.build_dir, which is
+  "   available as b:vimtex.compiler.build_dir.
+  " * Fallback to the main root directory
+  for l:root in [
+        \ $VIMTEX_OUTPUT_DIRECTORY,
+        \ get(get(self, 'compiler', {}), 'build_dir', ''),
+        \ self.root
+        \]
+    if empty(l:root) | continue | endif
+
+    let l:cand = printf('%s/%s.%s', l:root, self.name, a:ext)
+    if !vimtex#paths#is_abs(l:root)
+      let l:cand = self.root . '/' . l:cand
+    endif
+
+    if a:0 > 0 || filereadable(l:cand)
+      return fnamemodify(l:cand, ':p')
+    endif
+  endfor
+
+  return ''
+endfunction
+
+" }}}1
+function! s:vimtex.log() abort dict " {{{1
+  return self.ext('log')
+endfunction
+
+" }}}1
+function! s:vimtex.aux() abort dict " {{{1
+  return self.ext('aux')
+endfunction
+
+" }}}1
+function! s:vimtex.fls() abort dict " {{{1
+  return self.ext('fls')
+endfunction
+
+" }}}1
+function! s:vimtex.out(...) abort dict " {{{1
+  return call(self.ext, ['pdf'] + a:000, self)
+endfunction
+
+" }}}1
+
+
+function! s:parse_tex_program(preamble) abort " {{{1
+  let l:lines = copy(a:preamble[:20])
+  let l:tex_program_re =
+        \ '\v^\c\s*\%\s*!?\s*tex\s+%(ts-)?program\s*\=\s*\zs.*\ze\s*$'
+  call map(l:lines, 'matchstr(v:val, l:tex_program_re)')
+  call filter(l:lines, '!empty(v:val)')
+  return tolower(get(l:lines, -1, '_'))
+endfunction
+
+" }}}1
+function! s:parse_documentclass(preamble) abort " {{{1
+  let l:preamble_lines = filter(copy(a:preamble), {_, x -> x !~# '^\s*%'})
+  return matchstr(join(l:preamble_lines, ''),
+        \ '\\documentclass[^{]*{\zs[^}]\+\ze}')
+endfunction
+
+" }}}1
+function! s:parse_packages(preamble) abort " {{{1
+  let l:usepackages = filter(copy(a:preamble),
+        \ 'v:val =~# ''\v%(usep|RequireP)ackage''')
+  let l:pat = g:vimtex#re#not_comment . g:vimtex#re#not_bslash
+      \ . '\v\\%(usep|RequireP)ackage\s*%(\[[^[\]]*\])?\s*\{\s*\zs%([^{}]+)\ze\s*\}'
+  call map(l:usepackages, {_, x -> split(matchstr(x, l:pat), '\s*,\s*')})
+
+  let l:parsed = {}
+  for l:packages in l:usepackages
+    for l:package in l:packages
+      let l:parsed[l:package] = {}
+    endfor
+  endfor
+
+  return l:parsed
+endfunction
+
+" }}}1
+function! s:parse_graphicspath(preamble, root) abort " {{{1
+  " Combine the preamble as one long string of commands
+  let l:preamble = join(map(copy(a:preamble),
+        \ {_, x -> substitute(x, '\\\@<!%.*', '', '')}))
+
+  " Extract the graphicspath command from this string
+  let l:graphicspath = matchstr(l:preamble,
+          \ g:vimtex#re#not_bslash
+          \ . '\\graphicspath\s*\{\s*\{\s*\zs.{-}\ze\s*\}\s*\}'
+          \)
+
+  " Add all parsed graphicspaths
+  let l:paths = []
+  for l:path in split(l:graphicspath, '\s*}\s*{\s*')
+    let l:path = substitute(l:path, '\/\s*$', '', '')
+    call add(l:paths, vimtex#paths#is_abs(l:path)
+          \ ? l:path
+          \ : simplify(a:root . '/' . l:path))
+  endfor
+
+  return l:paths
+endfunction
+
+" }}}1
+function! s:gather_sources(texfile, root) abort " {{{1
+  let l:sources = vimtex#parser#tex#parse_files(
+        \ a:texfile, {'root' : a:root})
+
+  return map(l:sources, 'vimtex#paths#relative(v:val, a:root)')
+endfunction
+
+" }}}1

autoload/vimtex/compiler.vim
@@ -67,7 +67,7 @@ function! vimtex#compiler#callback(status) abort " {{{1
   endif
 
   if a:status && exists('b:vimtex')
-    call b:vimtex.parse_packages()
+    call b:vimtex.update_packages()
     call vimtex#syntax#packages#init()
   endif
 

autoload/vimtex/compiler/_template.vim
@@ -57,6 +57,45 @@ function! s:compiler.__build_cmd() abort dict " {{{1
 endfunction
 
 " }}}1
+function! s:compiler.__pprint() abort dict " {{{1
+  let l:list = []
+
+  if self.target_path !=# b:vimtex.tex
+    call add(l:list, ['root', self.root])
+    call add(l:list, ['target', self.target_path])
+  endif
+
+  if has_key(self, 'get_engine')
+    call add(l:list, ['engine', self.get_engine()])
+  endif
+
+  if has_key(self, 'options')
+    call add(l:list, ['options', self.options])
+  endif
+
+  if !empty(self.build_dir)
+    call add(l:list, ['build_dir', self.build_dir])
+  endif
+
+  if has_key(self, '__pprint_append')
+    call extend(l:list, self.__pprint_append())
+  endif
+
+  if has_key(self, 'job')
+    let l:job = []
+    call add(l:job, ['jobid', self.job])
+    call add(l:job, ['output', self.output])
+    call add(l:job, ['cmd', self.cmd])
+    if self.continuous
+      call add(l:job, ['pid', self.get_pid()])
+    endif
+    call add(l:list, ['process', l:job])
+  endif
+
+  return l:list
+endfunction
+
+" }}}1
 
 function! s:compiler.clean(full) abort dict " {{{1
   let l:files = ['synctex.gz', 'toc', 'out', 'aux', 'log']
@@ -70,13 +109,6 @@ function! s:compiler.clean(full) abort dict " {{{1
         \ self.build_dir, fnamemodify(self.target_path, ':t:r:S'), x)})
   call vimtex#process#run('rm -f ' . join(l:files))
   call vimtex#log#info('Compiler clean finished')
-endfunction
-
-" }}}1
-function! s:compiler.cleanup() abort dict " {{{1
-  if self.is_running()
-    call self.kill()
-  endif
 endfunction
 
 " }}}1
@@ -144,45 +176,6 @@ function! s:compiler.wait() abort dict " {{{1
   endfor
 
   call self.stop()
-endfunction
-
-" }}}1
-function! s:compiler.pprint_items() abort dict " {{{1
-  let l:list = []
-
-  if self.target_path !=# b:vimtex.tex
-    call add(l:list, ['root', self.root])
-    call add(l:list, ['target', self.target_path])
-  endif
-
-  if has_key(self, 'get_engine')
-    call add(l:list, ['engine', self.get_engine()])
-  endif
-
-  if has_key(self, 'options')
-    call add(l:list, ['options', self.options])
-  endif
-
-  if !empty(self.build_dir)
-    call add(l:list, ['build_dir', self.build_dir])
-  endif
-
-  if has_key(self, '__pprint_append')
-    call extend(l:list, self.__pprint_append())
-  endif
-
-  if has_key(self, 'job')
-    let l:job = []
-    call add(l:job, ['jobid', self.job])
-    call add(l:job, ['output', self.output])
-    call add(l:job, ['cmd', self.cmd])
-    if self.continuous
-      call add(l:job, ['pid', self.get_pid()])
-    endif
-    call add(l:list, ['process', l:job])
-  endif
-
-  return l:list
 endfunction
 
 " }}}1

autoload/vimtex/info.vim
@@ -127,8 +127,8 @@ function! s:parse_dict(dict, indent, ...) abort " {{{1
     let l:indent += 1
   endif
 
-  let l:items = has_key(l:dict, 'pprint_items')
-        \ ? l:dict.pprint_items() : items(l:dict)
+  let l:items = has_key(l:dict, '__pprint')
+        \ ? l:dict.__pprint() : items(l:dict)
 
   return extend(l:entries, s:get_info(l:items, l:indent))
 endfunction

autoload/vimtex/process.vim
@@ -44,6 +44,15 @@ let s:process = {
       \ 'result' : '',
       \}
 
+function! s:process.__pprint() abort dict " {{{1
+  return [
+        \ ['pid', self.pid ? self.pid : '-'],
+        \ ['cmd', get(self, 'prepared_cmd', self.cmd)],
+        \]
+endfunction
+
+" }}}1
+
 function! s:process.run() abort dict " {{{1
   if self._do_not_run() | return | endif
 
@@ -66,16 +75,6 @@ function! s:process.stop() abort dict " {{{1
   call vimtex#process#run(l:cmd, {'background': 0})
 
   let self.pid = 0
-endfunction
-
-" }}}1
-function! s:process.pprint_items() abort dict " {{{1
-  let l:list = [
-        \ ['pid', self.pid ? self.pid : '-'],
-        \ ['cmd', get(self, 'prepared_cmd', self.cmd)],
-        \]
-
-  return l:list
 endfunction
 
 " }}}1

autoload/vimtex/state.vim
@@ -22,7 +22,7 @@ function! vimtex#state#init() abort " {{{1
     let b:vimtex = s:vimtex_states[l:id]
   else
     let b:vimtex_id = s:vimtex_next_id
-    let b:vimtex = s:vimtex.new(l:main, l:main_type, 0)
+    let b:vimtex = vimtex#state#class#new(l:main, l:main_type, 0)
     let s:vimtex_next_id += 1
     let s:vimtex_states[b:vimtex_id] = b:vimtex
   endif
@@ -42,7 +42,8 @@ function! vimtex#state#init_local() abort " {{{1
 
   if l:vimtex_id < 0
     let l:vimtex_id = s:vimtex_next_id
-    let l:vimtex = s:vimtex.new(l:filename, 'local file', l:preserve_root)
+    let l:vimtex = vimtex#state#class#new(
+          \ l:filename, 'local file', l:preserve_root)
     let s:vimtex_next_id += 1
     let s:vimtex_states[l:vimtex_id] = l:vimtex
 
@@ -169,6 +170,7 @@ function! vimtex#state#cleanup(id) abort " {{{1
 endfunction
 
 " }}}1
+
 
 function! s:get_main_id(main) abort " {{{1
   for [l:id, l:state] in items(s:vimtex_states)
@@ -486,10 +488,10 @@ function! s:get_main_choose(list) abort " {{{1
 endfunction
 
 " }}}1
+
 function! s:file_is_main(file) abort " {{{1
   if !filereadable(a:file) | return 0 | endif
 
-  "
   " Check if a:file is a main file by looking for the \documentclass command,
   " but ignore the following:
   "
@@ -546,248 +548,6 @@ function! s:findfiles_recursive(expr, path) abort " {{{1
     let l:dirs .= ',' . l:path
   endwhile
   return split(globpath(fnameescape(l:dirs), a:expr), '\n')
-endfunction
-
-" }}}1
-
-let s:vimtex = {}
-
-function! s:vimtex.new(main, main_parser, preserve_root) abort dict " {{{1
-  let l:new = deepcopy(self)
-  let l:new.root = fnamemodify(a:main, ':h')
-  let l:new.base = fnamemodify(a:main, ':t')
-  let l:new.name = fnamemodify(a:main, ':t:r')
-  let l:new.main_parser = a:main_parser
-
-  if a:preserve_root && exists('b:vimtex')
-    let l:new.root = b:vimtex.root
-    let l:new.base = vimtex#paths#relative(a:main, l:new.root)
-  endif
-
-  let l:ext = fnamemodify(a:main, ':e')
-  let l:new.tex = l:ext ==# 'tex' ? a:main : ''
-
-  "
-  " The preamble content is used to parse for the engine directive, the
-  " documentclass and the package list; we store it as a temporary shared
-  " object variable
-  "
-  if !empty(l:new.tex)
-    let l:new.preamble = vimtex#parser#preamble(l:new.tex, {
-          \ 'root' : l:new.root,
-          \})
-  else
-    let l:new.preamble = []
-  endif
-
-  call l:new.parse_tex_program()
-  call l:new.parse_documentclass()
-  call l:new.parse_graphicspath()
-  call l:new.gather_sources()
-
-  " Initialize state in submodules
-  let l:new.disabled_modules = get(s:, 'disabled_modules', [])
-  for l:mod in filter(
-        \ ['view', 'compiler', 'qf', 'toc', 'fold', 'context'],
-        \ 'index(l:new.disabled_modules, v:val) < 0')
-    call vimtex#{l:mod}#init_state(l:new)
-  endfor
-
-  call l:new.parse_packages()
-
-  unlet l:new.preamble
-  unlet l:new.new
-  return l:new
-endfunction
-
-" }}}1
-function! s:vimtex.cleanup() abort dict " {{{1
-  if exists('self.compiler.cleanup')
-    call self.compiler.cleanup()
-  endif
-
-  if exists('#User#VimtexEventQuit')
-    if exists('b:vimtex')
-      let b:vimtex_tmp = b:vimtex
-    endif
-    let b:vimtex = self
-    doautocmd <nomodeline> User VimtexEventQuit
-    if exists('b:vimtex_tmp')
-      let b:vimtex = b:vimtex_tmp
-      unlet b:vimtex_tmp
-    else
-      unlet b:vimtex
-    endif
-  endif
-
-  " Close quickfix window
-  silent! cclose
-endfunction
-
-" }}}1
-function! s:vimtex.parse_tex_program() abort dict " {{{1
-  let l:lines = copy(self.preamble[:20])
-  let l:tex_program_re =
-        \ '\v^\c\s*\%\s*!?\s*tex\s+%(ts-)?program\s*\=\s*\zs.*\ze\s*$'
-  call map(l:lines, 'matchstr(v:val, l:tex_program_re)')
-  call filter(l:lines, '!empty(v:val)')
-  let self.tex_program = tolower(get(l:lines, -1, '_'))
-endfunction
-
-" }}}1
-function! s:vimtex.parse_documentclass() abort dict " {{{1
-  let l:preamble_lines = filter(copy(self.preamble), {_, x -> x !~# '^\s*%'})
-  let self.documentclass = matchstr(join(l:preamble_lines, ''),
-        \ '\\documentclass[^{]*{\zs[^}]\+\ze}')
-endfunction
-
-" }}}1
-function! s:vimtex.parse_graphicspath() abort dict " {{{1
-  " Combine the preamble as one long string of commands
-  let l:preamble = join(map(copy(self.preamble),
-        \ {_, x -> substitute(x, '\\\@<!%.*', '', '')}))
-
-  " Extract the graphicspath command from this string
-  let l:graphicspath = matchstr(l:preamble,
-          \ g:vimtex#re#not_bslash
-          \ . '\\graphicspath\s*\{\s*\{\s*\zs.{-}\ze\s*\}\s*\}'
-          \)
-
-  " Add all parsed graphicspaths
-  let self.graphicspath = []
-  for l:path in split(l:graphicspath, '\s*}\s*{\s*')
-    let l:path = substitute(l:path, '\/\s*$', '', '')
-    call add(self.graphicspath, vimtex#paths#is_abs(l:path)
-          \ ? l:path
-          \ : simplify(self.root . '/' . l:path))
-  endfor
-endfunction
-
-" }}}1
-function! s:vimtex.parse_packages() abort dict " {{{1
-  let self.packages = get(self, 'packages', {})
-
-  " Try to parse .fls file if present, as it is usually more complete. That is,
-  " it contains a generated list of all the packages that are used.
-  for l:line in vimtex#parser#fls(self.fls())
-    let l:package = matchstr(l:line, '^INPUT \zs.\+\ze\.sty$')
-    let l:package = fnamemodify(l:package, ':t')
-    if !empty(l:package)
-      let self.packages[l:package] = {}
-    endif
-  endfor
-
-  " Now parse preamble as well for usepackage and RequirePackage
-  if !has_key(self, 'preamble') | return | endif
-  let l:usepackages = filter(copy(self.preamble),
-        \ 'v:val =~# ''\v%(usep|RequireP)ackage''')
-  let l:pat = g:vimtex#re#not_comment . g:vimtex#re#not_bslash
-      \ . '\v\\%(usep|RequireP)ackage\s*%(\[[^[\]]*\])?\s*\{\s*\zs%([^{}]+)\ze\s*\}'
-  call map(l:usepackages, {_, x -> split(matchstr(x, l:pat), '\s*,\s*')})
-
-  for l:packages in l:usepackages
-    for l:package in l:packages
-      let self.packages[l:package] = {}
-    endfor
-  endfor
-endfunction
-
-" }}}1
-function! s:vimtex.gather_sources() abort dict " {{{1
-  let self.sources = vimtex#parser#tex#parse_files(
-        \ self.tex, {'root' : self.root})
-
-  call map(self.sources, 'vimtex#paths#relative(v:val, self.root)')
-endfunction
-
-" }}}1
-function! s:vimtex.pprint_items() abort dict " {{{1
-  let l:items = [
-        \ ['name', self.name],
-        \ ['base', self.base],
-        \ ['root', self.root],
-        \ ['tex', self.tex],
-        \ ['out', self.out()],
-        \ ['log', self.log()],
-        \ ['aux', self.aux()],
-        \ ['fls', self.fls()],
-        \ ['main parser', self.main_parser],
-        \]
-
-  if self.tex_program !=# '_'
-    call add(l:items, ['tex program', self.tex_program])
-  endif
-
-  if len(self.sources) >= 2
-    call add(l:items, ['source files', self.sources])
-  endif
-
-  call add(l:items, ['compiler', get(self, 'compiler', {})])
-  call add(l:items, ['viewer', get(self, 'viewer', {})])
-  call add(l:items, ['qf', get(self, 'qf', {})])
-
-  if exists('self.documentclass')
-    call add(l:items, ['document class', self.documentclass])
-  endif
-
-  if !empty(self.packages)
-    call add(l:items, ['packages', sort(keys(self.packages))])
-  endif
-
-  return [['VimTeX project', l:items]]
-endfunction
-
-" }}}1
-function! s:vimtex.log() abort dict " {{{1
-  return self.ext('log')
-endfunction
-
-" }}}1
-function! s:vimtex.aux() abort dict " {{{1
-  return self.ext('aux')
-endfunction
-
-" }}}1
-function! s:vimtex.fls() abort dict " {{{1
-  return self.ext('fls')
-endfunction
-
-" }}}1
-function! s:vimtex.out(...) abort dict " {{{1
-  return call(self.ext, ['pdf'] + a:000, self)
-endfunction
-
-" }}}1
-function! s:vimtex.ext(ext, ...) abort dict " {{{1
-  " Check for various output directories
-  " * Environment variable VIMTEX_OUTPUT_DIRECTORY. Note that this overrides
-  "   any VimTeX settings like g:vimtex_compiler_latexmk.build_dir!
-  " * Compiler settings, such as g:vimtex_compiler_latexmk.build_dir, which is
-  "   available as b:vimtex.compiler.build_dir.
-  " * Fallback to the main root directory
-  for l:root in [
-        \ $VIMTEX_OUTPUT_DIRECTORY,
-        \ get(get(self, 'compiler', {}), 'build_dir', ''),
-        \ self.root
-        \]
-    if empty(l:root) | continue | endif
-
-    let l:cand = printf('%s/%s.%s', l:root, self.name, a:ext)
-    if !vimtex#paths#is_abs(l:root)
-      let l:cand = self.root . '/' . l:cand
-    endif
-
-    if a:0 > 0 || filereadable(l:cand)
-      return fnamemodify(l:cand, ':p')
-    endif
-  endfor
-
-  return ''
-endfunction
-
-" }}}1
-function! s:vimtex.getftime() abort dict " {{{1
-  return max(map(copy(self.sources), 'getftime(self.root . ''/'' . v:val)'))
 endfunction
 
 " }}}1

autoload/vimtex/view/_template.vim
@@ -13,6 +13,26 @@ endfunction
 
 let s:template = {}
 
+function! s:template.__pprint() abort dict " {{{1
+  let l:list = []
+
+  if has_key(self, 'xwin_id')
+    call add(l:list, ['xwin id', self.xwin_id])
+  endif
+
+  if has_key(self, 'process')
+    call add(l:list, ['process', self.process])
+  endif
+
+  for l:key in filter(keys(self), 'v:val =~# ''^cmd_''')
+    call add(l:list, [l:key, self[l:key]])
+  endfor
+
+  return l:list
+endfunction
+
+" }}}1
+
 function! s:template.out() dict abort " {{{1
   return g:vimtex_view_use_temp_files
         \ ? b:vimtex.root . '/' . b:vimtex.name . '_vimtex.pdf'
@@ -44,25 +64,6 @@ function! s:template.copy_files() dict abort " {{{1
   if getftime(l:old) > getftime(l:new)
     call rename(l:old, l:new)
   endif
-endfunction
-
-" }}}1
-function! s:template.pprint_items() abort dict " {{{1
-  let l:list = []
-
-  if has_key(self, 'xwin_id')
-    call add(l:list, ['xwin id', self.xwin_id])
-  endif
-
-  if has_key(self, 'process')
-    call add(l:list, ['process', self.process])
-  endif
-
-  for l:key in filter(keys(self), 'v:val =~# ''^cmd_''')
-    call add(l:list, [l:key, self[l:key]])
-  endfor
-
-  return l:list
 endfunction
 
 " }}}1

commit 8519d49d2275c37f2377ef49198c7b72f019400c

fix: load packages from fls on init
This fixes a regression from commit 92d268a.

refer: #2170

autoload/vimtex/state/class.vim
@@ -30,6 +30,9 @@ function! vimtex#state#class#new(main, main_parser, preserve_root) abort " {{{1
   let l:new.graphicspath = s:parse_graphicspath(l:preamble, l:new.root)
   let l:new.sources = s:gather_sources(l:new.tex, l:new.root)
 
+  " Update package list from fls file (if available)
+  call l:new.update_packages()
+
   " Initialize state in submodules
   let l:new.disabled_modules = get(s:, 'disabled_modules', [])
   for l:mod in filter(